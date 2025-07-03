893450c617733334ae579003b9fd0930dba05918
Sabbir Bin Abdul Latif <sabbir0727@gmail.com>
2025-07-03 19:46:05 +0600

    refactor: Remove allowedHeaders from CORS configuration in app.ts

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,6 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.urlencoded({ extended: true }));
